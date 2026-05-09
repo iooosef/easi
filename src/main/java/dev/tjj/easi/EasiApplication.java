7dfe7e9a74b38b041e95f20bb3c8b432f52e2aa5
iooosef <josence22@gmail.com>
2026-05-10 00:19:03 +0800

    init commit

---
+++ src/main/java/dev/tjj/easi/EasiApplication.java
@@ -0,0 +1,13 @@
+package dev.tjj.easi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tjj/easi/EasiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tjj.easi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
