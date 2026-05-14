edc89b9b530bff2bcdf243439e442b2a1997cfa5
iooosef <josence22@gmail.com>
2026-05-15 05:24:23 +0800

    Installed and configured tailwind and flyonui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    outDir: '../src/main/resources/static',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  }
+
 })
